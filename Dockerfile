6c10c95887a79c44dc60286a9a4a2de944c59323
JayChen <admin@trj.tw>
2019-11-13 16:41:19 +0800

    update env port

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:lts-alpine
 WORKDIR /data
+ENV PORT 10230
 COPY . .
 RUN npm i --prod
-EXPOSE 10230
+EXPOSE ${PORT}
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Koa = require('koa')
 const app = new Koa()
 const os = require('os')
 
-app.listen(10230)
+app.listen(process.env.PORT || 10230)
 
 app.use(async c => {
   c.body = os.hostname()
